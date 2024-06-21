1cb602cd747d46338f6c249506e2aee2fec420e7
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-06-21 16:45:33 +0200

    inté commandes

--- front/src/components/account/order/OrderItemComponent.vue
+++ front/src/components/account/order/OrderItemComponent.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="order-item">
-    {{ props.order }}
+    <div class="order-item__content">
+      <div class="order-item__date">
+        <span class="success badge">
+          Livré
+        </span>
+        le 14 mai
+      </div>
+      <div class="order-item__id">
+        <span>ID de commande #{{ props.order.order_id }}</span>
+      </div>
+      <p class="order-item__description">
+        Votre coli est arrivé à destination au Adresse, Ville, Pays.
+      </p>
+    </div>
+    <div class="order-item__ctas">
+      <button class="button" @click="console.log('clicked')">
+        Détails
+      </button>
+      <button class="button outline" @click="console.log('clicked')">
+        Voir le produit
+      </button>
+    </div>
   </div>
 </template>
 
@@ -22,9 +43,30 @@ const props = defineProps({
   box-shadow: 3px 3px 15px 3px rgba(0, 0, 0, 0.1);
   border-radius: 25px;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
   gap: 1rem;
   padding: 1rem;
+}
+
+.order-item__date {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.25rem 0;
+  font-size: 1rem;
+  font-weight: 700;
+}
+
+.order-item__id {
+  font-weight: 700;
+  color: var(--gray);
+}
+
+.order-item__ctas {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: space-between;
 }
 </style>
